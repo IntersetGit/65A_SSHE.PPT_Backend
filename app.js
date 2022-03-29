9d0e75b91f4f55568b49da9959861368e9fb9de8
sriharach <folksriharach@gmail.com>
2022-03-29 12:47:21 +0700

    demo

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ require('./socket')
 require('./cron')
 
 const indexRouter = require('./routes/index');
+const authenticationRouter = require('./routes/authen');
 
 app.use(cors());
 // app.use(helmet());
@@ -29,6 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/auth', authenticationRouter);
 
 swagger.serveSwagger(app, "/api", options, {
   routePath: "./routes/",
